a575c77cfe6d12a3ad52be9a9fba3f509943d97a
valendutour <valen.dutour@gmail.com>
2023-09-18 21:18:13 -0300

    1ciclo-1condicional-1funcion

---
+++ main.js
@@ -0,0 +1,25 @@
+let servicio= true
+
+const pregunta = prompt("¿Queres que te vendamos un producto? Si o No")
+while(servicio== true){
+    if(pregunta == "Si"){
+
+    const producto = prompt("Por favor ingresa el nombre del producto que quieras que vendamos")
+    console.log(producto)
+    
+    const precio = parseInt(prompt("Por favor asignale un valor a tu producto. Recordá que se le descontara el costo por el servicio que es de $200"))
+    console.log(precio)
+    const costoServicio = 200
+
+    function Ganancia(){
+    let ganancia= precio - costoServicio
+    return ganancia
+    }
+    alert("Su ganancia total será de $" + Ganancia())
+
+    }else{
+    alert("Tu respuesta no fue Si. Para vender un producto refresca la pagina.")
+    }
+
+    servicio=confirm("Queres agregar otro producto?")
+}
