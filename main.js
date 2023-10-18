811d79447f38f0344954d241655323b4cf5b893f
valendutour <valen.dutour@gmail.com>
2023-10-18 20:17:06 -0300

    hice uso del DOM, del localStorage y converti datos a json con el metodo stringify

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
-let name = document.getElementById("name");
-let mail = document.getElementById("mail");
-let celu = document.getElementById("celu");
-let comment = document.getElementById("comment");
-let send = document.getElementById("send");
-let clear = document.getElementById("clear");
+const nombre = document.getElementById("inputNombre");
+const mail = document.getElementById("inputEmail");
+const celu = document.getElementById("inputTel");
+const comment = document.getElementById("floatingTextarea2");
+const send = document.getElementById("boton");
+const clear = document.getElementById("clear");
+
+function guardar(){
+    if (nombre.value.trim() !== "" && mail.value.includes("@") && !isNaN(celu.value) && comment.value.trim() !== "") {
+
+    const jsonNombre = JSON.stringify(nombre.value)
+    const jsonMail = JSON.stringify(mail.value)
+    const jsonCelu = JSON.stringify(celu.value)
+    const jsonComment = JSON.stringify(comment.value)
+
+    localStorage.setItem('name',jsonNombre)
+    localStorage.setItem("mail",jsonMail)
+    localStorage.setItem('celu',jsonCelu)
+    localStorage.setItem('comment',jsonComment)
+
+    console.log(typeof nombre)
+    console.log(typeof jsonNombre)
+
+    }else {
+        alert("Por favor, complete todos los campos correctamente.");
+    }
+}
+
+send.addEventListener("click",guardar)
+
+
+/*
+if("name"==="" || "mail"==="" || isNaN(celu)){
+ que aparezca cartel que diga rellena todos los campos
+<div id="ventanaModal" class="modal">
+ <div class="modal-contenido">
+     <span class="cerrar" id="cerrarModal">&times;</span>
+     <h2>Producto Agregado</h2>
+     <p>Se ha agregado un nuevo producto a la lista.</p>
+ </div>
+</div>
+}
+
+<!--start link a pag 404 error href="./404.html" (estaba puesto en el boton enviar)end-->
 
 send.addEventListener("click",enviar);
 
@@ -11,3 +49,4 @@ function enviar(x){
     x.preventDefault();
     console.log("formulario enviado")
 }
+*/
